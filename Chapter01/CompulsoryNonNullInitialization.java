f8d44d0d7c57bb3a59cc8fc679e89d27f733f13f
malagupta <mala.gupta@gmail.com>
2019-03-25 18:57:16 +0530

    Chap1 code - part 1

---
+++ Chapter01/CompulsoryNonNullInitialization.java
@@ -0,0 +1,6 @@
+public class CompulsoryNonNullInitialization {
+    public static void main(String[] args) {
+        var minAge;                   // uninitialized variable
+        var age = null;               // variable assigned a null value
+    }
+}
---
+++ Chapter01/TypeInferenceWithVar1.java
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+public class TypeInferenceWithVar1 {
+    public static void main(String[] args) {
+        String name = "Java Everywhere";
+        LocalDateTime dateTime = LocalDateTime.now();
+        HashMap<Integer, String> map = new HashMap<Integer, String>();
+    }
+}
